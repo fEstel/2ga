e37d7188c843c281577ab2fcb5a30f358c0d0327
root <root@localhost.localdomain>
2012-11-01 23:26:49 +0900

    delete at project list

--- apps/frontend/modules/project/templates/indexSuccess.php
+++ apps/frontend/modules/project/templates/indexSuccess.php
@@ -16,6 +16,7 @@
       </thead>
       <tbody>
         <?php foreach ($ide_projects as $ide_project): ?>
+         <?php if(!$ide_project->getDeletedAt()): ?>
           <tr>
             <td><a href="<?php echo url_for('project/show?id=' . $ide_project->getId()) ?>"><?php echo $ide_project->getName() ?></a></td>
             <td><?php echo $ide_project->getDescription() ?></td>
@@ -27,7 +28,9 @@
             <td><a href="<?php echo url_for('editor/index?project=' . $ide_project->getId()) ?>">Edit</a></td>
             <td><a href="<?php echo url_for("http://" . $ide_project->getName() . "." . $myname . "." . TogaSettings::getServerDomain()) ?>">Demo</a></td>
             <td><a href="<?php echo url_for("http://" . $ide_project->getName() . "." . $myname . "." . TogaSettings::getServerDomain()) . "/frontend_dev.php" ?>">Debug</a></td>
+            <td><a href="/project/delete/id/6" onclick="if (confirm('Are you sure?')) { var f = document.createElement('form'); f.style.display = 'none'; this.parentNode.appendChild(f); f.method = 'post'; f.action = this.href;var m = document.createElement('input'); m.setAttribute('type', 'hidden'); m.setAttribute('name', 'sf_method'); m.setAttribute('value', 'delete'); f.appendChild(m);var m = document.createElement('input'); m.setAttribute('type', 'hidden'); m.setAttribute('name', '_csrf_token'); m.setAttribute('value', '1e25346399661ef5a8c592588a2ba159'); f.appendChild(m);f.submit(); };return false;">Delete</a></td>
           </tr>
+          <?php endif;?>
         <?php endforeach; ?>
       </tbody>
     </table>
